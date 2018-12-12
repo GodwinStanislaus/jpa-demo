fb84fc78ac4d7d2db877c8f99a4939f41a2ec2b3
Godwin Stanislaus <godwinjosewesley@gmail.com>
2018-12-12 16:17:12 +0530

    H2 db added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into labtest
+values(1,'Blood Test');
+
+insert into labtest
+values(2,'Glucose Test');
+
+insert into labtest
+values(3,'Water Test');
---
+++ src/test/resources/data.sql
@@ -0,0 +1,8 @@
+insert into labtest
+values(4,'Blood Test');
+
+insert into labtest
+values(5,'Glucose Test');
+
+insert into labtest
+values(6,'Water Test');
